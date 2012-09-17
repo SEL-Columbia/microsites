948faa78fcb8eeae9cdefac5d898af4668e7396f
renaud gaudin <rgaudin@gmail.com>
2012-09-17 10:23:59 +0000

    trigger field with group

--- soillab/views.py
+++ soillab/views.py
@@ -122,7 +122,7 @@ def form_splitter(request, project_slug='soildoc'):
     # we don't submit empty forms to formhub.
     # must be a suffixed field!
     AVAIL_SUFFIXES = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-    empty_trigger = 'sample_barcode_id'
+    empty_trigger = 'sample_id/sample_barcode_id'
 
     # map field suffixes with IDs in holder
     # we exclude forms with no data on trigger field so it won't be process
